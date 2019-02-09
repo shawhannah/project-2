c3b44a52e6a372881b4421d5391b1a1268ae7f88
hanshaw <hshaw@alumni.unc.edu>
2019-02-09 11:45:26 -0500

    sql files

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,18 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS ipa_db;
+CREATE DATABASE ipa_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+USE ipa_db;
+
+CREATE TABLE ipa
+(
+    id int NOT NULL
+    AUTO_INCREMENT, 
+beer varchar
+    (255) NOT NULL, 
+brewery varchar
+    (255) NOT NULL, 
+type varchar
+    (255) NOT NULL, 
+PRIMARY KEY
+    (id)
+);
---
+++ models/seeds.sql
@@ -0,0 +1,13 @@
+INSERT INTO ipa
+    (beer)
+VALUES
+    ("Pernicious");
+INSERT INTO ipa
+    (beer)
+VALUES
+    ("Hoppyum");
+INSERT INTO ipa
+    (beer)
+VALUES
+    ("Space Dust");
+
