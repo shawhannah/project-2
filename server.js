97cc99202e96de855bd84e63e1736644cac2815c
hanshaw <hshaw@alumni.unc.edu>
2019-02-19 18:44:35 -0500

    half way through passport setup

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var exphbs = require("express-handlebars");
 var db = require("./models");
 
 var app = express();
+var passport   = require('passport')
+var session    = require('express-session')
+var bodyParser = require('body-parser')
+
+var env = require('dotenv').load();
+
 var PORT = process.env.PORT || 3000;
 
 // Passport
@@ -15,12 +21,37 @@ app.get('/', function(req, res) {
 });
 
 
-app.listen(3000, function(err) {
+app.listen(5000, function(err) {
 
   if (!err)
       console.log("Site is live");
   else console.log(err)
 
+});
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+ 
+app.use(passport.initialize());
+ 
+app.use(passport.session()); // persistent login sessions
+
+//Models
+var models = require("./models");
+ 
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+    console.log('Nice! Database looks fine')
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "Something went wrong with the Database Update!")
+ 
 });
 
 // Middleware
